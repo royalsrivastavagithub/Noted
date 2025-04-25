a0d33c54f862cee09ca3f29c6aba4e5e20277d1f
Royal Srivastava <157229547+royalsrivastavagithub@users.noreply.github.com>
2025-04-25 22:56:22 +0530

    reduced asset size

--- client/src/components/login-form.tsx
+++ client/src/components/login-form.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import img from "@/assets/Noted.png";
+import img from "@/assets/Noted.jpg";
 import axios from "axios";
 import React, { useState } from "react";
 import {
