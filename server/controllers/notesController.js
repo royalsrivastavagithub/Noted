f154ba4cb565578045e5ea0633ab1b58e0e8a868
Royal Srivastava <157229547+royalsrivastavagithub@users.noreply.github.com>
2025-04-26 13:22:16 +0530

    fixing imports

--- server/controllers/notesController.js
+++ server/controllers/notesController.js
@@ -1,4 +1,4 @@
-const User = require("../models/User"); // Import the User model
+const User = require("../models/user"); // Import the User model
 const { encrypt, decrypt } = require("../utils/encryption");
 
 // Create a new note
